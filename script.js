ba687025841d8cd776d69aca95f23a06df39e61b
Krishnath-Sirivel <krishnath.sirivel@ontariotechu.net>
2026-03-07 17:44:02 -0500

    Implement cursor‑following circle animation

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const circle = document.querySelector(".circle");
 
+document.addEventListener("mousemove", (event) => {
+  circle.style.left = event.clientX + "px";
+  circle.style.top = event.clientY + "px";
+});
