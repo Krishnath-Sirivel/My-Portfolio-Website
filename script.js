56caaed13d53090ff40ce8839df34ebff6625d61
Krishnath-Sirivel <krishnath.sirivel@ontariotechu.net>
2026-05-05 16:33:29 -0400

    Remove button and script tags from script.js
    Removed button and script tags from script.js.

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-<button onclick="changeColor()">Change Background</button>
-
-<script>
 function changeColor() {
     document.body.style.backgroundColor = 
         '#' + Math.floor(Math.random()*16777215).toString(16);
 }
-</script>
