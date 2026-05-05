0d9e528153dd76dce3daebf5e8a4b0b77efb49f0
Krishnath-Sirivel <krishnath.sirivel@ontariotechu.net>
2026-05-05 16:46:08 -0400

    Add resetColor function to reset background color

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function changeColor() {
     document.body.style.backgroundColor = 
         '#' + Math.floor(Math.random()*16777215).toString(16);
 }
+
+function resetColor() {
+    document.body.style.backgroundColor = '#000000';
+}
